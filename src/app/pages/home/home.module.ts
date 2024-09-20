002fe90b77e096873ff11fe13105e9b5d008781f
ran1.987 <rakeshranjan406c@gmail.com>
2024-09-20 14:02:57 +0530

    made correction path

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -2,7 +2,7 @@ import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core'
 import { RouterModule } from '@angular/router'
 import { CommonModule } from '@angular/common'
 
-import { ComponentsModule } from '../components/components.module'
+import { ComponentsModule } from '../../components/components.module'
 import { Home } from './home.component'
 
 const routes = [
--- src/app/pages/not-found/not-found.module.ts
+++ src/app/pages/not-found/not-found.module.ts
@@ -2,7 +2,7 @@ import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core'
 import { RouterModule } from '@angular/router'
 import { CommonModule } from '@angular/common'
 
-import { ComponentsModule } from '../components/components.module'
+import { ComponentsModule } from '../../components/components.module'
 import { NotFound } from './not-found.component'
 
 const routes = [
